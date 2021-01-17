ae797a382714d502b47eca3b695b85bc27f473c2
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2021-01-17 14:30:40 +0500

    updates

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function getRandNumber(imgId) {
+function getRandNumber(imgclass) {
   var randNumber = Math.floor(Math.random() * 6 + 1);
-  document.getElementById(imgId).src =
+  document.getElementsByClassName(imgclass)[0].src =
     "images/dice" + randNumber + ".png";
   return randNumber;
 }
 
-var playerScore1 = getRandNumber("img_payer_1_id");
-var playerScore2 = getRandNumber("img_payer_2_id");
+var playerScore1 = getRandNumber("img_payer_1");
+var playerScore2 = getRandNumber("img_payer_2");
 
-var titleText = document.getElementById("refresh_me_title_id");
+var titleText = document.getElementsByClassName("refresh_me_title")[0];
 if (playerScore1 > playerScore2) {
   titleText.innerText = "Player 1 wins";
 } else if (playerScore1 < playerScore2) {
